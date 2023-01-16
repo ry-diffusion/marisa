bb512bfe875358142a78b798621a3e49b9b2fd46
yxqsnz <yxqsnz@gmail.com>
2023-01-16 20:53:44 -0300

    feat: clicking

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
-use std::{
-    sync::mpsc::{self, Receiver},
-    thread::{self, JoinHandle},
-};
+use std::thread;
 
 use cli::Marisa;
-use evdev::{Device, InputEventKind, Key};
+use evdev::{
+    uinput::VirtualDeviceBuilder, AttributeSet, Device, EventType, InputEvent, InputEventKind, Key,
+};
 use utils::find_mouse_devices;
 
 mod cli;
 mod utils;
 
 fn listen(mut device: Device) -> color_eyre::Result<()> {
+    let mut attribs = AttributeSet::<Key>::new();
+    attribs.insert(Key::BTN_LEFT);
+    attribs.insert(Key::BTN_RIGHT);
+    let mut dev = VirtualDeviceBuilder::new()?
+        .name(format!("Marisa for: {}", device.name().unwrap_or("unknown")).as_str())
+        .with_keys(&attribs)?
+        .build()?;
+
     loop {
         for event in device.fetch_events()? {
-            if event.value() == 0 {
-                continue;
-            }
+            // if event.value() == 0 {
+            //     continue;
+            // }
 
             if let InputEventKind::Key(key) = event.kind() {
                 if matches!(key, Key::BTN_LEFT | Key::BTN_RIGHT) {
-                    println!("*Click*");
+                    for _ in 0..10 {
+                        std::thread::sleep(std::time::Duration::from_millis(15));
+                        let event = InputEvent::new(EventType::KEY, key.0, 1);
+                        dev.emit(&[event])?;
+
+                        std::thread::sleep(std::time::Duration::from_millis(15));
+                        let event = InputEvent::new(EventType::KEY, key.0, 0);
+                        dev.emit(&[event])?;
+                    }
                 }
             }
         }
